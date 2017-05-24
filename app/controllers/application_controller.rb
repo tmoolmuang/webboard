2afb0f8881302afe8c6c4cb6599bd85966863a28
Thanya Moolmuang <tmoolmuang@yahoo.com>
2017-05-24 17:34:13 +0000

    fix confirm email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,19 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :role, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :role, :password, :password_confirmation, :current_password) }
   end
   
+  def error_messages(records)
+    err = "Error! "
+    records.errors.each do |attribute, message|
+      err = err + " " + message 
+    end
+    return err
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def error_messages(records)
-    err = "Error! "
-    records.errors.each do |attribute, message|
-      err = err + " " + message 
-    end
-    return err
-  end
-
 end
