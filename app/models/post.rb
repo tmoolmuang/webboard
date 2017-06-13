a1ae114bc13e34497ca734d153dd0582b65cbd94
Thanya Moolmuang <tmoolmuang@yahoo.com>
2017-06-13 23:23:30 +0000

    mod layout

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
   
-  validates :body, presence: { message: 'Post body cannot be emptied.' }
+  validates :body, presence: { message: 'Post body cannot be emptied.' }, length: { maximum: 175, message: 'Post body is limited to 175 characters.' }
   validates :category, presence: { message: 'A Post must have associated category.' }
   validates :user, presence: { message: 'A Post must have associated user.' }
 end
